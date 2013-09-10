971eddba4e10a46dfb3ed51f76db7f6b1529c2fa
killy971 <killy971@gmail.com>
2013-09-10 23:17:35 +0900

    Fixed SynchronousInvocationHandler and added a test

--- java/ahilib/src/main/java/com/github/guillaumenargeot/ahilib/proxy/Synchronous.java
+++ java/ahilib/src/main/java/com/github/guillaumenargeot/ahilib/proxy/Synchronous.java
@@ -1,7 +1,5 @@
 package com.github.guillaumenargeot.ahilib.proxy;
 
-import com.google.common.util.concurrent.Futures;
-
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -41,13 +39,11 @@ public final class Synchronous {
             if (!method.getReturnType().equals(Future.class)) {
                 throw new UnsupportedOperationException("");
             }
-            final Future futureResult;
             try {
-                futureResult = (Future) method.invoke(asynchronousProxy, objects);
+                return method.invoke(asynchronousProxy, objects);
             } catch (InvocationTargetException e) {
                 throw e.getTargetException();
             }
-            return Futures.getUnchecked(futureResult);
         }
     }
 }
